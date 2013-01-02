dc9d7657e6a3dfdc77d63bfeb63ecff506935179
Roman Brodetskiy <roman.brodetskiy@e-legion.com>
2013-01-02 23:13:34 +0400

    layout

--- assets/js/board.js
+++ assets/js/board.js
@@ -1,3 +1,11 @@
-$(function(e){
-	
+$(function(e) {
+	var canvas = document.getElementById('canvas');
+	var context = canvas.getContext('2d');
+	var imageObj = new Image();
+
+	imageObj.onload = function() {
+		context.drawImage(imageObj, 69, 50);
+	};
+	imageObj.src = '/images/board.jpg';
+
 })

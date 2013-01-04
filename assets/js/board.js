064aafd2202f40ec562a190046bb5e5baa4478c7
Roman Brodetskiy <roman.brodetskiy@e-legion.com>
2013-01-04 14:04:48 +0400

    jquery.svg

--- assets/js/board.js
+++ assets/js/board.js
@@ -2,10 +2,12 @@ Board = (function(e) {
 	var c;
 	var LEFT = 10
 	var TOP = 100
-	var EDGE = 55
+	var EDGE = 44
+	var WALL_LENGTH = 90
 	var white;
 	var black;
-	
+
+
 	function col(p) {
 		return p.charCodeAt(0) - 'a'.charCodeAt(0) + 1
 	}
@@ -15,50 +17,48 @@ Board = (function(e) {
 	}
 
 	function pawn(pawn, target) {
-		
+
 	}
 
 	function wall(p) {
-		c.lineWidth = 6;
-		c.beginPath();
-		left = LEFT + col (p) * EDGE
-		top_ = TOP + row(p) * EDGE
-		if(p[2] == 'h') {
-			left -= EDGE
-		} else {
-			top_ -= EDGE
-		}
-		c.moveTo(left, top_);
-		c.lineTo(left + (p[2] == 'v' ? 0 : EDGE * 2), top_ + (p[2] == 'h' ? 0 : EDGE * 2));
-		// c.lineTo(col(p) * 10 + 10, row(p) * 10 + 10);
-		c.stroke();
+		svg.line(0, 0, 100, 130, {
+			strokeWidth: 5
+		});
+
 	}
+
+	function init_walls() {
+
+		for(var i = 0; i < 10; i++) {
+			svg.line(EDGE * i, 0, EDGE * i, 100, {
+				stroke: "black",
+				strokeWidth: 5
+			});
+
+			svg.line(EDGE * i, 500 , EDGE * i, 600, {
+				stroke: "black",
+				strokeWidth: 5
+			});
+
+		}
+	}
+
+	function init(svg) {
+		window.svg = svg
+		var back = svg.image(0, 0, 400, 600, 'images/board.svg');
+		
+		init_walls()
+	}
+
+	var setup = function() {
+			$('#svg').svg({
+				onLoad: init
+			});
+
+		}
 
 	return {
-		setup: function() {
-			var canvas = document.getElementById('canvas');
-			c = canvas.getContext('2d');
-			var imageObj = new Image();
-
-			imageObj.onload = function() {
-				c.drawImage(imageObj, 0, 0);
-				wall("a1v")
-				wall("b2v")
-				wall("f3h")
-				c.beginPath();
-				c.fillStyle = '#fff';
-				white = c.arc(LEFT + EDGE * 4.5, TOP + EDGE * 0.5, 10, 0, Math.PI * 2, true);
-				c.closePath()
-				c.fill()
-				c.beginPath();
-				black = c.arc(LEFT + EDGE * 4.5, TOP + EDGE * 8.5, 10, 0, Math.PI * 2, true);
-				c.closePath();
-				c.fillStyle = '#000';
-				c.fill()
-
-			};
-			imageObj.src = '/images/board.jpg';
-
-		}
+		setup: setup
 	}
+
 })()
